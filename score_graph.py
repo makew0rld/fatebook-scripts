979d9d08c0e7046c0dd79920f2d772041f89bd68
makeworld <makeworld@protonmail.com>
2025-01-21 20:16:20 -0500

    parse time better, print summary, improve colours

--- score_graph.py
+++ score_graph.py
@@ -27,6 +27,7 @@ HEADERS = [
 
 def parse_time(ts: str) -> datetime:
     # https://github.com/Sage-Future/fatebook/issues/90
+    ts = ts if len(ts.split(":")[-1]) == 2 else ts[:-1] + "0" + ts[-1]
     return datetime.strptime(ts, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
 
 
@@ -64,6 +65,9 @@ with open(sys.argv[1]) as f:
 # Sort the data by creation time
 questions.sort(key=lambda q: q.created_at)
 
+# for q in questions:
+#     print(q.created_at.date(), q.score)
+
 # Calculate the averages now that the data is sorted
 
 # Average of scores for each question and all preceding ones
@@ -88,26 +92,29 @@ def rolling_average(days: int) -> List[Tuple[datetime, float]]:
 
 
 three_month = rolling_average(91)
-one_month = rolling_average(30)
+
+three_month_latest = three_month[-1][1]
+avg_latest = total_averages[-1]
+
+print("Resolved questions:", len(questions))
+print("Three month average:", three_month_latest)
+print("Total average:", avg_latest)
 
 fig, ax = plt.subplots()
 ax.set_ylim(0, 2)
 
 line_kwargs = {"marker": None, "markersize": 3}
-# ax.plot(
-#     *zip(*one_month),
-#     label=f"1-month rolling average ({one_month[-1][1]:.2f})",
-#     **line_kwargs,
-# )
 ax.plot(
     *zip(*three_month),
-    label=f"3-month rolling average (latest: {three_month[-1][1]:.2f})",
+    label=f"3-month rolling average (latest: {three_month_latest:.2f})",
+    color="tab:blue",
     **line_kwargs,
 )
 ax.plot(
     [q.created_at for q in questions],  # type: ignore
     total_averages,
-    label=f"total average (latest: {total_averages[-1]:.2f})",
+    label=f"total average (latest: {avg_latest:.2f})",
+    color="tab:orange",
     **line_kwargs,
 )
 
@@ -120,6 +127,7 @@ ax.scatter(
     label=f"question scores (total: {len(questions)})",
     marker="o",
     s=5,
+    c="tab:green",
 )
 
 plt.legend()
